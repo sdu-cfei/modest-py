3ff7f9a32fded4cea3a999472bb506321e621d51
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-09-27 13:00:29 +0200

    Enclose tests in "if __name__ == "__main__""

--- bin/test.py
+++ bin/test.py
@@ -2,5 +2,6 @@
 from modestpy.test import run
 from modestpy.loginit import config_logger
 
-config_logger(filename='unit_tests.log', level='DEBUG')
-run.tests()
+if __name__ == "__main__":
+    config_logger(filename='unit_tests.log', level='DEBUG')
+    run.tests()
